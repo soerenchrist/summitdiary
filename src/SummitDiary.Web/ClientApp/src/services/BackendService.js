7406f29e0a5beef6e9f64a7d033bb4608737ce2a
Sören Christ <soeren.christ@azo.com>
2021-07-15 09:30:59 +0200

    Refactor backendservice

--- src/SummitDiary.Web/ClientApp/src/services/BackendService.js
+++ src/SummitDiary.Web/ClientApp/src/services/BackendService.js
@@ -2,52 +2,57 @@ import axios from 'axios';
 
 const baseUrl = '/api/';
 
+const client = axios.create({
+  baseURL: baseUrl,
+  json: true,
+});
+
 export default {
   async getCountries() {
-    const response = await axios.get(`${baseUrl}countries`);
+    const response = await client.get('countries');
     return response.data;
   },
   async getRegions() {
-    const response = await axios.get(`${baseUrl}regions`);
+    const response = await client.get('regions');
     return response.data;
   },
   async createSummit(summit) {
-    const response = await axios.post(`${baseUrl}summits`, summit);
+    const response = await client.post('summits', summit);
     return response.data;
   },
   async createRegion(region) {
-    const response = await axios.post(`${baseUrl}regions`, region);
+    const response = await client.post('regions', region);
     return response.data;
   },
   async createCountry(country) {
-    const response = await axios.post(`${baseUrl}countries`, country);
+    const response = await client.post('countries', country);
     return response.data;
   },
   async createActivity(activity) {
-    const response = await axios.post(`${baseUrl}activities`, activity);
+    const response = await client.post('activities', activity);
     return response.data;
   },
   async getActivity(activityId) {
-    const response = await axios.get(`${baseUrl}activities/${activityId}`);
+    const response = await client.get(`activities/${activityId}`);
     return response.data;
   },
   async getSummitImage(summitId) {
-    const response = await axios.get(`${baseUrl}summits/${summitId}/image`);
+    const response = await client.get(`summits/${summitId}/image`);
     return response.data;
   },
   async getActivityPath(activityId) {
-    const response = await axios.get(`${baseUrl}activities/${activityId}/path`);
+    const response = await client.get(`activities/${activityId}/path`);
     return response.data;
   },
   async getSummitById(summitId) {
-    const response = await axios.get(`${baseUrl}summits/${summitId}`);
+    const response = await client.get(`summits/${summitId}`);
     return response.data;
   },
   async deleteSummit(summitId) {
-    await axios.delete(`${baseUrl}summits/${summitId}`);
+    await client.delete(`summits/${summitId}`);
   },
   async deleteActivity(activityId) {
-    await axios.delete(`${baseUrl}activities/${activityId}`);
+    await client.delete(`activities/${activityId}`);
   },
   async getPagedSummits(options) {
     let {
@@ -77,7 +82,7 @@ export default {
       [sortDesc] = sortDesc;
     }
 
-    let url = `${baseUrl}summits?pageNumber=${page}&pageSize=${itemsPerPage}&sortBy=${sortBy}&sortDescending=${sortDesc}`;
+    let url = `summits?pageNumber=${page}&pageSize=${itemsPerPage}&sortBy=${sortBy}&sortDescending=${sortDesc}`;
 
     if (options.searchText) {
       url += `&searchText=${options.searchText}`;
@@ -97,7 +102,7 @@ export default {
       url += `&swLat=${swLat}&swLon=${swLon}&neLat=${neLat}&neLon=${neLon}`;
     }
 
-    const response = await axios.get(url);
+    const response = await client.get(url);
     return response.data;
   },
   async getPagedActivities(options) {
@@ -132,7 +137,7 @@ export default {
       [sortDesc] = sortDesc;
     }
 
-    let url = `${baseUrl}activities?pageNumber=${page}&pageSize=${itemsPerPage}&sortBy=${sortBy}&sortDescending=${sortDesc}`;
+    let url = `activities?pageNumber=${page}&pageSize=${itemsPerPage}&sortBy=${sortBy}&sortDescending=${sortDesc}`;
 
     if (options.searchText) {
       url += `&searchText=${options.searchText}`;
@@ -142,7 +147,7 @@ export default {
       url = `${url}&summitId=${summitId}`;
     }
 
-    const response = await axios.get(url);
+    const response = await client.get(url);
     return response.data;
   },
   async uploadGpx(activityId, file) {
@@ -150,46 +155,46 @@ export default {
     formData.append('file', file, file.name);
     formData.append('activityId', activityId);
 
-    const response = await axios.post(`${baseUrl}activities/${activityId}/gpx`, formData);
+    const response = await client.post(`activities/${activityId}/gpx`, formData);
     return response.data;
   },
   async analyzeGpx(file) {
     const formData = new FormData();
     formData.append('file', file, file.name);
 
-    const response = await axios.post(`${baseUrl}gpx/analyze`, formData);
+    const response = await client.post('gpx/analyze', formData);
     return response.data;
   },
   async getTimeline(options) {
     const { valueType, timeType } = options;
-    const response = await axios.get(`${baseUrl}stats/timeline?valueType=${valueType}&timeType=${timeType}`);
+    const response = await client.get(`stats/timeline?valueType=${valueType}&timeType=${timeType}`);
     return response.data;
   },
   async getCountryStats(options) {
     const { valueType } = options;
-    const response = await axios.get(`${baseUrl}stats/country?valueType=${valueType}`);
+    const response = await client.get(`stats/country?valueType=${valueType}`);
     return response.data;
   },
   async getTotals() {
-    const response = await axios.get(`${baseUrl}stats/totals`);
+    const response = await client.get('stats/totals');
     return response.data;
   },
   async getWishlistItemForSummit(summitId) {
-    const response = await axios.get(`${baseUrl}summits/${summitId}/wishlist`);
+    const response = await client.get(`summits/${summitId}/wishlist`);
     return response.data;
   },
   async addSummitToWishlist(summitId) {
-    const response = await axios.post(`${baseUrl}wishlist`, {
+    const response = await client.post('wishlist', {
       summitId,
     });
 
     return response.data;
   },
   async removeFromWishlist(wishlistItemId) {
-    await axios.delete(`${baseUrl}wishlist/${wishlistItemId}`);
+    await client.delete(`wishlist/${wishlistItemId}`);
   },
   async getWishlist() {
-    const response = await axios.get(`${baseUrl}wishlist`);
+    const response = await client.get('wishlist');
     return response.data;
   },
 };
